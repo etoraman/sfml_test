43afa174c1392f501c81865d403d248bfd006d66
etoraman <emretoraman@yandex.com>
2015-10-24 13:05:35 +0300

    main.cpp
    sfml dll proje klasöründe olmalı

---
+++ main.cpp
@@ -0,0 +1,70 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <iostream>
+using namespace std;
+using namespace sf;
+int main()
+{
+	cout << "basladi";
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+	for (int k = 0; k < Joystick::Count; k++){
+		bool connected = sf::Joystick::isConnected(k);
+		if (connected){
+
+			cout << endl << "Joystick : " << k << " connected " ;
+			cout << endl << " button count "<< sf::Joystick::getButtonCount(k) << endl;
+		}
+	}
+	
+	
+
+	while (window.isOpen())
+	{
+		for (int k = 0; k < Joystick::Count; k++){
+
+			for (int i = 0; i < (Joystick::getButtonCount(k)); i++){
+				if (sf::Joystick::isButtonPressed(k, i)){
+					cout << endl <<"Joystick"<< k << ": button" << i << "pressed" << endl;
+						//Joystick::getAxisPosition 	( 	unsigned int  	joystick,Axis  	axis)		
+				}
+				int AxisX = Joystick::getAxisPosition(k, Joystick::X);
+				int AxisY = Joystick::getAxisPosition(k, Joystick::Y);
+				if (AxisX == 100){
+					cout << endl << "saga";
+				}
+				else if (AxisX == -100){
+					cout << endl << "sola";
+				}
+				if (AxisY == 100){
+					cout << endl << "geri";
+				}
+				else if (AxisY == -100){
+					cout << endl << "ileri";
+				}
+				
+				if (AxisX <= 100 && AxisX >= -100 && AxisX != 0){
+					cout << endl << AxisX;
+				}
+				if (AxisY <= 100 && AxisY >= -100 && AxisY != 0){
+					cout << endl << AxisY;
+				}		
+			}
+
+		}
+
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
